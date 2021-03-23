7f525a08505722a77177a75fa9a5767a70c2f826
nickbevers <nick.bevers9@gmail.com>
2021-03-23 09:22:10 +0100

    Adds cors to the requirements so request can go through

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const routerMessages = require("./routes/api/v1/index")
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   app.set("view engine", "pug");
